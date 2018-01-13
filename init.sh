5605a4f24b765bc8f2f7cd6de37069028ed1f088
MykolaKlishch <35305198+MykolaKlishch@users.noreply.github.com>
2018-01-13 19:44:57 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,2 @@
-sudo service nginx start
-sudo unlink /etc/nginx/sites-enabled/default
-sudo ln -sfv /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-nano /var/log/nginx/error.log
