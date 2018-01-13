188910d425a1146429da46fc9db26092e8fca39f
MykolaKlishch <35305198+MykolaKlishch@users.noreply.github.com>
2018-01-13 19:05:25 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,3 @@
 sudo unlink /etc/nginx/sites-enabled/default
 sudo ln -sfv /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
-sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
