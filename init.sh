dba5367a225e15a01a35cb5f6e39c8db549d4ef6
MykolaKlishch <35305198+MykolaKlishch@users.noreply.github.com>
2018-01-19 22:24:16 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo apt-get install gunicorn
 sudo ln -sf /home/box/web/etc/gunicorn.conf.py  /etc/gunicorn.d/test
+gunicorn -c /home/box/web/hello.py hello:application
 sudo service nginx restart
-gunicorn -c hello.py hello:application
