5156739c9b69e02b87d44ce323d94c4e8b501f8e
MykolaKlishch <35305198+MykolaKlishch@users.noreply.github.com>
2018-01-20 14:33:24 +0200

    Update _init_1_8_12.sh

--- _init_1_8_12.sh
+++ _init_1_8_12.sh
@@ -1,2 +1,2 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx_1_8_12.conf  /etc/nginx/sites-enabled/default
 sudo service nginx restart
