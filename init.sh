e2e6312ed366f1fd61c917eab8bfc7359fb698bd
MykolaKlishch <35305198+MykolaKlishch@users.noreply.github.com>
2018-01-20 13:09:39 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,9 @@
-sudo apt-get install gunicorn
-sudo ln -sf /home/box/web/etc/gunicorn.conf.py  /etc/gunicorn.d/test
-gunicorn -c /etc/gunicorn.d/test test:application
-sudo service nginx restart
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn.conf.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
+
+#sudo apt-get install gunicorn
+#sudo ln -sf /home/box/web/etc/gunicorn.conf.py  /etc/gunicorn.d/test
+#gunicorn -c /etc/gunicorn.d/test test:application
+#sudo service nginx restart
