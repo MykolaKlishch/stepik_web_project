36f1785212f95f6881548d9c87ffa05b61990d98
MykolaKlishch <35305198+MykolaKlishch@users.noreply.github.com>
2018-01-20 13:43:53 +0200

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 def hello_func(env, start_response):
     
     status = '200 OK'
-    heasers = [('Content-Type', 'text/plain')]
+    headers = [('Content-Type', 'text/plain')]
     start_response(status, headers)
     
     body = [bytes(q + '\n', ascii) for q in env[QUERY_STRING].split('&')]
