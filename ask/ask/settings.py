13131e6fbd15f56361f2015eac955193097e4fe0
MykolaKlishch <35305198+MykolaKlishch@users.noreply.github.com>
2018-01-26 21:43:28 +0200

    Update settings.py

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'vue++2hy6h0ansi#688l2s+6xkez0e60w5j&1otwj6y$+28ms!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # original - ALLOWED_HOSTS = ['*']
 
 
 # Application definition
