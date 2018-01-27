8775f72f9131eb1d21064821687af2db1c841dfd
MykolaKlishch <35305198+MykolaKlishch@users.noreply.github.com>
2018-01-27 22:44:52 +0200

    Update init2111.sh

--- init2111.sh
+++ init2111.sh
@@ -2,6 +2,6 @@ sudo ln -sf /home/box/web/etc/nginx2111.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn_conf_django.py /etc/gunicorn.d/gunicorn_conf
 sudo /etc/init.d/gunicorn restart
-cd ./box/web/ask
-gunicorn ask.wsgi:application --bind 111.222.333.44:8000
+cd /home/box/web/ask                                                            
+gunicorn ask.wsgi:application --bind 0.0.0.0:8000
 # gunicorn -b 0.0.0.0:8000 wsgi & 
